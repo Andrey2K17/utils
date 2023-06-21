13791e43db24acc84f226a024bc44d48bfd71c16
= <nba.grinin.96@gmail.com>
2023-06-21 22:40:54 +0700

    init commit

---
+++ app/src/main/java/com/pg13/myutils/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pg13.myutils
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
